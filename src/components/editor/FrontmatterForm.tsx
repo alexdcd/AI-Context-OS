9e6189c48d8e4a09b33394e88b3227b4e9670d0b
alexdcd <hello@alexdc.net>
2026-04-10 03:51:19 +0200

    refactor: remove unused MEMORY_ONTOLOGY_LABELS import from FrontmatterForm

--- src/components/editor/FrontmatterForm.tsx
+++ src/components/editor/FrontmatterForm.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import type { MemoryMeta, MemoryOntology } from "../../lib/types";
-import { MEMORY_ONTOLOGY_LABELS } from "../../lib/types";
 
 interface FrontmatterFormProps {
   meta: MemoryMeta;
